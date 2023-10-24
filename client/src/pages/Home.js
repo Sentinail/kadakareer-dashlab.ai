a50458ce05dabcd9912656b801474c22c24bd2a7
Sentinail <wilsongponseca@gmail.com>
2023-10-24 23:21:56 +0800

    Fixed issue with CSS used as a style, Changed scroll to top behavior

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,80 +1,146 @@
-import React, { useEffect, useState } from 'react'
-import { SectionContainer, ItemCenter, Footer } from '../styled-components/HomeStyle'
+import React, { useEffect, useState } from "react";
+import {
+	SectionContainer,
+	ItemCenter,
+	Footer,
+	BreakpointProvider,
+} from "../styled-components/HomeStyle";
 import { useTheme } from "../contexts/themeContext";
-import group from "../assets/group.png"
-import { faArrowUp } from '@fortawesome/free-solid-svg-icons';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import '../styled-components/Breakpoints.css'
+import group from "../assets/group.png";
+import { faArrowUp } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const Home = () => {
-  const { fourthColor } = useTheme()
-  const [isSmallScreen, setIsSmallScreen] = useState(window.innerWidth < 993)
+	const { fourthColor } = useTheme();
+	const [isSmallScreen, setIsSmallScreen] = useState(window.innerWidth < 993);
 
-  useEffect(() => {
-    const handleResize = () => {
-      const smallScreen = window.innerWidth < 993;
-      if (smallScreen !== isSmallScreen) {
-        setIsSmallScreen(smallScreen);
-      }
-    };
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth',
+    });
+  };
 
-    window.addEventListener('resize', handleResize);
+	useEffect(() => {
+		const handleResize = () => {
+			const smallScreen = window.innerWidth < 993;
+			if (smallScreen !== isSmallScreen) {
+				setIsSmallScreen(smallScreen);
+			}
+		};
 
-    return () => {
-      window.removeEventListener('resize', handleResize);
-    }
-  }, [isSmallScreen])
+		window.addEventListener("resize", handleResize);
 
-  return (
+		return () => {
+			window.removeEventListener("resize", handleResize);
+		};
+	}, [isSmallScreen]);
 
-    <>
+	return (
+		<>
+			<BreakpointProvider>
+				<div id="home">
+					<SectionContainer>
+						<ItemCenter>
+							<div className="imageContainer">
+								<img
+									src={
+										"https://dashlabs.ai/assets/images/image05.svg?v=4a9c1c0a"
+									}
+                  alt="dashlab_icon"
+								/>
+							</div>
+							<div className="textContainer">
+								<h1>
+									Automating Health / Medical Diagnostic Lab
+									Processes with {">"} 20x Speed Increase and
+									99.99% Accuracy
+								</h1>
+								<p>
+									Dashlabs.ai offers a single, end-to-end
+									platform for automating and optimizing
+									healthcare and medical diagnostic lab
+									operations including integration with any
+									lab machine.
+								</p>
+							</div>
+						</ItemCenter>
+					</SectionContainer>
+					<SectionContainer style={{ backgroundColor: fourthColor }}>
+						<ItemCenter>
+							<div className="textContainer2">
+								<h1>Team SkillBlend Synergy</h1>
+								<p>
+									Lorem ipsum dolor sit amet consectetur
+									adipisicing elit. Mollitia nulla
+									consequuntur rerum ab, eos officia quis
+									nobis tempore totam non maiores ipsam
+									adipisci nesciunt unde alias? Quia
+									cupiditate ducimus labore! Lorem ipsum dolor
+									sit amet consectetur adipisicing elit.
+									Mollitia nulla consequuntur rerum ab, eos
+									officia quis nobis tempore totam non maiores
+									ipsam adipisci nesciunt unde alias? Quia
+									cupiditate ducimus labore!
+								</p>
+							</div>
+							<div className="imageContainer2">
+								<img src={group} alt="Teamwork" />
+							</div>
+						</ItemCenter>
+					</SectionContainer>
+					<SectionContainer>
+						<div className="aboutSection">
+							<div className="title">
+								<h1>About Project</h1>
+							</div>
+							<div className="textContainer3">
+								<p>
+									Lorem ipsum dolor sit amet consectetur
+									adipisicing elit. Mollitia nulla
+									consequuntur rerum ab, eos officia quis
+									nobis tempore totam non maiores ipsam
+									adipisci nesciunt unde alias? Quia
+									cupiditate ducimus labore! Lorem ipsum dolor
+									sit amet consectetur adipisicing elit.
+									Mollitia nulla consequuntur rerum ab, eos
+									officia quis nobis tempore totam non maiores
+									ipsam adipisci nesciunt unde alias? Quia
+									cupiditate ducimus labore!Lorem ipsum dolor
+									sit amet consectetur adipisicing elit.
+									Mollitia nulla consequuntur rerum ab, eos
+									officia quis nobis tempore totam non maiores
+									ipsam adipisci nesciunt unde alias? Quia
+									cupiditate ducimus labore! Lorem ipsum dolor
+									sit amet consectetur adipisicing elit.
+									Mollitia nulla consequuntur rerum ab, eos
+									officia quis nobis tempore totam non maiores
+									ipsam adipisci nesciunt unde alias? Quia
+									cupiditate ducimus labore!
+								</p>
+							</div>
+						</div>
+					</SectionContainer>
+					<Footer>
+						<div>
+							<p>
+								Copyright © 2023 by Dashlab.ai | All Rights
+								Reserved.
+							</p>
+						</div>
+						<div className="iconTop">
+							<button className="scroll_top" onClick={scrollToTop}>
+								<FontAwesomeIcon
+									icon={faArrowUp}
+									size={isSmallScreen ? "xs" : null}
+								/>
+							</button>
+						</div>
+					</Footer>
+				</div>
+			</BreakpointProvider>
+		</>
+	);
+};
 
-      <div id='home'>
-        <SectionContainer>
-          <ItemCenter>
-            <div className='imageContainer'>
-              <img src={'https://dashlabs.ai/assets/images/image05.svg?v=4a9c1c0a'} />
-            </div>
-            <div className='textContainer'>
-              <h1>Automating Health / Medical Diagnostic Lab Processes with {'>'} 20x Speed Increase and 99.99% Accuracy</h1>
-              <p>Dashlabs.ai offers a single, end-to-end platform for automating and optimizing healthcare and medical diagnostic lab operations including integration with any lab machine.</p>
-            </div>
-          </ItemCenter>
-        </SectionContainer>
-        <SectionContainer style={{ backgroundColor: fourthColor }}>
-          <ItemCenter>
-            <div className='textContainer2'>
-              <h1>Team SkillBlend Synergy</h1>
-              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia nulla consequuntur rerum ab, eos officia quis nobis tempore totam non maiores ipsam adipisci nesciunt unde alias? Quia cupiditate ducimus labore! Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia nulla consequuntur rerum ab, eos officia quis nobis tempore totam non maiores ipsam adipisci nesciunt unde alias? Quia cupiditate ducimus labore!</p>
-            </div>
-            <div className='imageContainer2'>
-              <img src={group} alt='Teamwork' />
-            </div>
-          </ItemCenter>
-        </SectionContainer>
-        <SectionContainer>
-          <div className='aboutSection'>
-            <div className='title'>
-              <h1>About Project</h1>
-            </div>
-            <div className='textContainer3'>
-              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia nulla consequuntur rerum ab, eos officia quis nobis tempore totam non maiores ipsam adipisci nesciunt unde alias? Quia cupiditate ducimus labore! Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia nulla consequuntur rerum ab, eos officia quis nobis tempore totam non maiores ipsam adipisci nesciunt unde alias? Quia cupiditate ducimus labore!Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia nulla consequuntur rerum ab, eos officia quis nobis tempore totam non maiores ipsam adipisci nesciunt unde alias? Quia cupiditate ducimus labore! Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia nulla consequuntur rerum ab, eos officia quis nobis tempore totam non maiores ipsam adipisci nesciunt unde alias? Quia cupiditate ducimus labore!</p>
-            </div>
-          </div>
-        </SectionContainer>
-        <Footer>
-          <div>
-            <p>Copyright © 2023 by Dashlab.ai | All Rights Reserved.</p>
-          </div>
-          <div className='iconTop'>
-            <a href='#home'>
-              <FontAwesomeIcon icon={faArrowUp} size={isSmallScreen ? 'xs' : null} />
-            </a>
-          </div>
-        </Footer>
-      </div>
-    </>
-  )
-}
-
-export default Home
+export default Home;
--- client/src/styled-components/HomeStyle.js
+++ client/src/styled-components/HomeStyle.js
@@ -1,100 +1,199 @@
 import styled from "styled-components";
 
+export const BreakpointProvider = styled.div`
+    width: 100%;
+    height: auto;
+    
+	@media (width <= 1200px) {
+		.imageContainer img {
+			width: 255px !important;
+		}
+		.imageContainer2 img {
+			width: 255px !important;
+		}
+		.textContainer h1 {
+			font-size: 20px;
+		}
+		.textContainer p {
+			margin-top: 12px !important;
+			font-size: 14px;
+		}
 
+		.textContainer {
+			width: 100% !important;
+		}
 
+		.textContainer2 {
+			width: 100% !important;
+		}
+
+		.textContainer2 h1 {
+			font-size: 20px;
+		}
+		.textContainer2 p {
+			font-size: 14px;
+		}
+
+		.aboutSection .title h1 {
+			font-size: 23px;
+		}
+
+		.aboutSection .textContainer3 p {
+			width: 100% !important;
+			font-size: 13px;
+			line-height: 25px;
+		}
+	}
+
+	@media (width <= 992px) {
+		.iconTop a {
+			padding: 0.5rem !important;
+		}
+		.iconTop a i {
+			font-size: 10px !important;
+		}
+	}
+
+	@media (width <= 768px) {
+		section:first-child div {
+			flex-direction: column;
+		}
+
+		section:first-child div img {
+			width: 100% !important;
+			margin-top: -100px;
+			margin-bottom: 80px;
+			margin-right: 0 !important;
+		}
+
+		.imageContainer2 {
+			display: none;
+		}
+
+		.textContainer2 {
+			text-align: center;
+		}
+
+		.textContainer2 p {
+			margin-right: 0px !important;
+		}
+	}
+
+	@media (width <= 600px) {
+		footer {
+			flex-direction: column;
+			padding: 1rem 9% !important;
+		}
+
+		footer div p {
+			font-size: 13px;
+		}
+
+		footer .iconTop {
+			margin-top: 15px;
+		}
+	}
+
+	@media (width <= 450px) {
+		.dashlabs img {
+			width: 150px !important;
+		}
+	}
+`;
 
 export const SectionContainer = styled.section`
-    min-height: 100svh;
-    padding: 2rem 9% 2rem;
-    display: flex;
-    justify-content: center;
-    align-items: center;
+	min-height: 100svh;
+	padding: 2rem 9% 2rem;
+	display: flex;
+	justify-content: center;
+	align-items: center;
 
-    & .imageContainer img{
-        width: 455px;
-        margin-right: 50px
-    }
+	& .imageContainer img {
+		width: 455px;
+		margin-right: 50px;
+	}
 
-    .textContainer{
-        color: #FFFFFF;
-        width: 645px;
-    }
+	.textContainer {
+		color: #ffffff;
+		width: 645px;
+	}
 
-    .textContainer p{
-        opacity: 60%;
-        margin-top: 25px;
-    }
+	.textContainer p {
+		opacity: 60%;
+		margin-top: 25px;
+	}
 
-    .textContainer2{
-        color: #FFFFFF;
-        width: 645px;
-    }
+	.textContainer2 {
+		color: #ffffff;
+		width: 645px;
+	}
 
-    .textContainer2 p{
-        opacity: 60%;
-        margin-top: 20px;
-        line-height: 2rem;
-        margin-right: 40px
-    }
+	.textContainer2 p {
+		opacity: 60%;
+		margin-top: 20px;
+		line-height: 2rem;
+		margin-right: 40px;
+	}
 
-    .imageContainer2 img{
-        width: 455px;
-    }
-    
-    .aboutSection{
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-        justify-content: center;
-        color: #FFFFFF;
-    }
+	.imageContainer2 img {
+		width: 455px;
+	}
 
-    .aboutSection title h1{
-        text-align: center;
-    }
+	.aboutSection {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+		color: #ffffff;
+	}
 
-    .textContainer3 p{
-        opacity: 60%;
-        margin-top: 40px;
-        text-align: center;
-        width: 920px;
-    }
-    `
+	.aboutSection title h1 {
+		text-align: center;
+	}
+
+	.textContainer3 p {
+		opacity: 60%;
+		margin-top: 40px;
+		text-align: center;
+		width: 920px;
+	}
+`;
 
 export const ItemCenter = styled.div`
-
-    display: flex;
-    align-items: center;
-    justify-content: center;
-`
+	display: flex;
+	align-items: center;
+	justify-content: center;
+`;
 
 export const Footer = styled.footer`
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    flex-wrap: wrap;
-    padding: 2rem 9%;
-    background-color: #232D45;
-    opacity: 93%;
-    color: #ffff;
+	display: flex;
+	justify-content: space-between;
+	align-items: center;
+	flex-wrap: wrap;
+	padding: 2rem 9%;
+	background-color: #232d45;
+	opacity: 93%;
+	color: #ffff;
 
-    & div p{
-        opacity: 70%;
-    } 
-    
-    .iconTop a{
-        display: inline-flex;
-        justify-content: center;
-        align-items: center;
-        padding: .8rem;
-        background-color: #3e80c2;
-        border-radius: 0.3rem;
-        transition: .5s ease;
-        color: #ffffff;
-        font-size: 1.4rem;
-    }
+	& div p {
+		opacity: 70%;
+	}
 
-    .iconTop a:hover{
-        box-shadow: 0 0 1rem #4893df;
-    }
-`
+	.iconTop .scroll_top {
+        cursor: pointer;
+        border: none;
+		display: inline-flex;
+		justify-content: center;
+		align-items: center;
+		padding: 0.8rem;
+		background-color: #3e80c2;
+		border-radius: 0.3rem;
+		transition: 0.5s ease;
+		color: #ffffff;
+		font-size: 1.4rem;
+	}
+
+	.iconTop .scroll_top:hover {
+		box-shadow: 0 0 1rem #4893df;
+	}
+`;
